1ebe092afe213ee1d2dfb319158321d186110de3
Sharipov Nuriddin <sharipov.n1220@gmail.com>
2023-07-22 19:08:39 +0500

    To lear basic of leaflet

---
+++ data/test.js
@@ -0,0 +1,126 @@
+var data={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            69.21581339743537,
+            41.38207653008524
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            69.27355317204271,
+            41.38640870445735
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            69.30011346836255,
+            41.35954457093655
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            69.31050662779165,
+            41.33180221652373
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            69.33475733312548,
+            41.31012027826739
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            69.36131762944527,
+            41.29450481629715
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            69.37171078887442,
+            41.28235687196275
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            69.36940119788986,
+            41.26065848875453
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            69.35438885649296,
+            41.25024070323491
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            69.33706692411002,
+            41.238084520237834
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            69.3405313105869,
+            41.3916069327191
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
